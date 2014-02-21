cedbf4911ffbb09ed002c87794caad50059e4848
ncsnail <ncsnail@gmail.com>
2014-02-21 16:45:26 +0800

    servletTool

--- src/main/java/org/wind/sc/web/controller/user/UserController.java
+++ src/main/java/org/wind/sc/web/controller/user/UserController.java
@@ -1,6 +1,9 @@
 package org.wind.sc.web.controller.user;
 
+import javax.servlet.ServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,7 +13,9 @@ public class UserController {
 	
 	
 	@RequestMapping(value = "/user")
-	public String getList(){
+	public String getList(Model model,ServletRequest request){
+		
+		
 		
 		return "";
 	}
